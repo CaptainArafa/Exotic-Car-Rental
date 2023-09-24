9386cadf65389bb88b1e517d8f953d8ee877e2d2
Youssef <Sindermank29@gmail.com>
2023-09-24 22:52:49 +0300

    fixing classnames

--- src/Components/HomePageFront.jsx
+++ src/Components/HomePageFront.jsx
@@ -7,9 +7,9 @@ export const HomePageFront = ({scrollToSearch,rentalSearch}) => {
     <div className='Front-Container'>
         <div className='Front-Text'>
         <h4>Drive your dream car now</h4>
-        <div class="custom-shape-divider-top-1693413012">
+        <div className="custom-shape-divider-top-1693413012">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z" class="shape-fill"></path>
+        <path d="M741,116.23C291,117.43,0,27.57,0,6V120H1200V6C1200,27.93,1186.4,119.83,741,116.23Z" className="shape-fill"></path>
     </svg>
 </div>
         <h1 className='Front-Text-Main'>Rent <span>Exotic</span> cars with us today</h1>
